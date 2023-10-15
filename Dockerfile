9f6e58e566a150b35969a974a8f3a95f585ac654
Kim Seok Je <54321744+hwarang97@users.noreply.github.com>
2023-10-15 21:05:17 +0900

    Initialize FastAPI project setup (#1)
    Co-authored-by: KennethanCeyer <kenneth@pigno.se>

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.11
+
+ENV CONF_HOST 0.0.0.0
+ENV CONF_PORT 8000
+
+WORKDIR /app
+COPY . /app
+RUN pip install -r requirements.txt
+
+# A command for running FastAPI app.
+CMD ["python3", "main.py"]
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+import uvicorn
+
+
+app = FastAPI()
+
+
+class Settings(BaseSettings):
+    conf_host: str = "0.0.0.0"
+    conf_port: int = 8000
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+def run_app() -> None:
+    uvicorn.run(app, host=settings.conf_host, port=settings.conf_port)
+
+
+def main() -> None:
+    run_app()
+
+
+if __name__ == "__main__":
+    main()
