ef832662f3a1853521c69cee3587e050dfb809b1
KennethanCeyer <kenneth@pigno.se>
2023-10-15 20:26:43 +0900

    feat: add code running style fastapi

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from pydantic_settings import BaseSettings
+import uvicorn
+
 
 app = FastAPI()
+
+class Settings(BaseSettings):
+    conf_host: str = "0.0.0.0"
+    conf_port: str = "8000"
+
+
+settings = Settings()
+
 
 @app.get('/')
 def read_root():
     return {'Hello': 'World'}
+
+def run_app() -> None:
+    uvicorn.run(app, host=settings.conf_host, port=settings.conf_port)
+
+
+def main() -> None:
+    run_app()
+
+
+if __name__ == "__main__":
+    main()
+
 
