d9b09d948de4ab9793889fd7c745b91f90eafdd4
KennethanCeyer <kenneth@pigno.se>
2023-10-15 21:01:32 +0900

    chore: turn off other lints rather than flake8 and black and removing test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def gugu(a: int) -> None:
-    for i in range(1, 9+1):
-        ans = a * i
-        print(f"{a=} {i=} = {ans=}")
-def main() -> None:
-    gugu(5)
-if __name__ == "__main__":
-    main()
