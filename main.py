d16266816c80581a28ce8bd3e7931841e900615c
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-31 00:48:26 +0900

    refactor: move database connection setup from model.py to main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
+
 import uvicorn
+from fastapi import Depends
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
+DATABASE_URL = os.environ.get("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Settings(BaseSettings):
@@ -29,6 +37,14 @@ def run_app() -> None:
         port=settings.conf_port,
         reload=settings.conf_debug,
     )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def main() -> None:
