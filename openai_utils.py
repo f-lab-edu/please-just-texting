00e0cf288a9812db97db4f53ec3c0eb4e4eea284
root <root@DESKTOP-9MPN88P.localdomain>
2023-11-01 13:06:43 +0900

    chore: change code to official code

--- openai_utils.py
+++ openai_utils.py
@@ -1,18 +1,21 @@
 import os
 
 import openai
-from dotenv import load_dotenv
 
-# load .env file
-load_dotenv()
-
-# API key setting
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
+completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    prompt="Translate the following English text to Korean: 'Hello, how are you?'",
-    max_tokens=150,
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+        },
+        {
+            "role": "user",
+            "content": "Compose a poem that explains the concept of recursion in programming.",
+        },
+    ],
 )
 
-print(response.choices[0].text.strip())
+print(completion.choices[0].message)
