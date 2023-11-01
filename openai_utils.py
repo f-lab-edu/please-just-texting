20ccff55564b07df142e0f7b0c66ad57ec556f37
root <root@DESKTOP-9MPN88P.localdomain>
2023-11-01 12:56:48 +0900

    chore: chagne prompt message

--- openai_utils.py
+++ openai_utils.py
@@ -10,7 +10,9 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
-    model="gpt-3.5-turbo", prompt="message", max_tokens=150
+    model="gpt-3.5-turbo",
+    prompt="Translate the following English text to Korean: 'Hello, how are you?'",
+    max_tokens=150,
 )
 
 print(response.choices[0].text.strip())
