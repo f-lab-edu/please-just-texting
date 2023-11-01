fe65470989f9395c450356f79f1748ce5d0142ef
root <root@DESKTOP-9MPN88P.localdomain>
2023-11-01 12:55:02 +0900

    feat: add dotenv to load openai api key from .env

--- openai_utils.py
+++ openai_utils.py
@@ -1,9 +1,13 @@
 import os
 
 import openai
+from dotenv import load_dotenv
+
+# load .env file
+load_dotenv()
 
 # API key setting
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
     model="gpt-3.5-turbo", prompt="message", max_tokens=150
