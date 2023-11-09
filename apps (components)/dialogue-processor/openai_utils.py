8c56751e8727d2d7ce547729244785c557e40982
root <root@DESKTOP-9MPN88P>
2023-11-10 03:56:03 +0900

    feat: update promt for openai api

--- apps (components)/dialogue-processor/openai_utils.py
+++ apps (components)/dialogue-processor/openai_utils.py
@@ -12,11 +12,11 @@ conversation = os.getenv("CONVERSATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 messages = [
-    {"role": "system", "content": "이 문장에서 약속 날짜를 년-월-일 형식으로 추출해줘."},
-    {"role": "user", "content": conversation},
     {
         "role": "system",
-        "content": "생각: 대화내역을 통해 참여자들이 만나려고 하는 일정을 파악하려한다. 행동: 일정과 관련되어 보이는 키워드와 문맥을 파악한다. 관찰: 참여자들 중 대다수가 참여의사를 밝히는 날짜를 파악한다.",
+        "content": "너는 회의 발화 내용을 읽고 일정을 잡아주는 비서 시스템이야.\
+       앞으로 문장이 주어질 텐데, 그 문장에서 일정으로 추가해야하는 유저의 특정 일자 제목과 예상되는 일자의 아젠다 그리고 마지막으로 일자(YYYY-MM-DD) 형식으로 추출해야 해야해.\
+       상대적 일자 (내일, 다음주, 어제, 그저께 등은 오늘 일자 '2023-11-01' 기준으로 계산해줘.)",
     },
 ]
 
