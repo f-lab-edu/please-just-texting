42fc9ac9db86b41cb79bc2c6820bb63dd4206840
root <root@DESKTOP-9MPN88P>
2023-11-11 18:20:53 +0900

    feat: add code using google calendar api

--- app/utils/calender_utils.py
+++ app/utils/calender_utils.py
@@ -1,45 +1,53 @@
-import os.path
+import datetime
+import os
 import pickle
 
-import datatime
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-# google calendar api setting
+# 캘린더 API 접근 권한 설정
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-creds = None
-if os.path.exists("token.pickle"):
-    with open("token.pickle", "rb") as token:
-        creds = pickle.load(token)
 
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            "path_to_your_client_secrets.json", SCOPES
-        )
-        creds = flow.run_local_server(port=0)
+def get_calendar_service():
+    creds = None
+    # lode token file
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+
+    # 자격증명이 유효하지 않거나 존재하지 않으면 새로 생성
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # 새로운 자격증명을 token.pickle 파일에 저장
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
-# google calendar api object
-service = build("calendar", "v3", credentials=creds)
+    service = build("calendar", "v3", credentials=creds)
+    return service
 
-event = {
-    "summary": "테스트 일정",
-    "description": "Google Calendar API를 사용한 테스트 일정",
-    "start": {
-        "dataTime": "2023-11-05T09:00:00-07:00",
-        "timeZone": "Asia/Seoul",
-    },
-    "end": {
-        "dateTime": "2023-11-05T10:00:00-07:00",
-        "timeZone": "Asia/Seoul",
-    },
-}
 
-event = service.events().insert(calendarID="primary", body=event).execute()
-print(f'일정 추가되었습니다: {event["htmlLink"]}')
+def add_event_to_calendar(date, title, description):
+    service = get_calendar_service()
+
+    event = {
+        "summary": title,
+        "description": description,
+        "start": {
+            "dateTime": f"{date}T09:00:00",
+            "timeZone": "Asia/Seoul",
+        },
+        "end": {
+            "dateTime": f"{date}T10:00:00",
+            "timeZone": "Asia/Seoul",
+        },
+    }
+
+    event = service.events().insert(calendarId="primary", body=event).execute()
+    print(f"Event created: {event.get('htmlLink')}")
