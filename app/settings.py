2b6640787293878292073735863139c694859ee0
dolggul <yonunim@gmail.com>
2023-11-22 21:57:10 +0900

    fix: remove type hint on Settings class in settings.py

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSetting: BaseSettings) -> None:
+class Settings(BaseSetting: BaseSettings):
     openai_api_key: str = Field(..., env="OPENAI_API_KEY")
     conversation: str = Field(..., env="CONVERSATION")
 
