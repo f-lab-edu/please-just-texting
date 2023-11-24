506a9a76f206525995a904c77da541207b287ada
dolggul <yonunim@gmail.com>
2023-11-24 22:50:36 +0900

    fix: remove type hint on User class in models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class User(Base: base):
+class User(Base):
     __tablename__ = "users"
     id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
     name = Column(String(50))
