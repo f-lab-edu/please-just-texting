307053ee1a96a044d51752aa3c3fc0506e3e545a
dolggul <yonunim@gmail.com>
2023-12-20 19:51:28 +0900

    fix: delete type hint on setttings var

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
-settings: BaseSettings = Settings()
+settings = Settings()
 
 
 @app.get("/", response_class=HTMLResponse)
