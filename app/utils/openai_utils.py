94afb0e75931241baee010c6d4bbefd93bec0b65
dolggul <yonunim@gmail.com>
2023-12-29 10:52:01 +0900

    refacotr: remove comments in openai_tuils.py

--- app/utils/openai_utils.py
+++ app/utils/openai_utils.py
@@ -20,5 +20,4 @@ def getResponseFromOpenai(message: str) -> str:
     ]
 
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
-    # return response.choices[0].message.content.strip()
-    return response.choices
+    return response.choices[0].message.content.strip()
