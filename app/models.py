70ae21ed4e7e78d0d0bc952bee6dc3f6abc73ae0
dolggul <yonunim@gmail.com>
2024-02-01 16:25:31 +0900

    fix(models.py): correct typo

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Integer
 from sqlalchemy import Sequence
 from sqlalchemy import String
-from sqlalchemy.orm import Declarative_Base
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 
-class Base(Declarative_Base):
+class Base(DeclarativeBase):
     pass
 
 
@@ -16,7 +16,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     password_hash: Mapped[str] = mapped_column(String(100))
-    email: Mapped[str] = mapped_column(String(100), unique=True, nallable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
