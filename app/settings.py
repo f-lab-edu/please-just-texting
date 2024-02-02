856a37133bc097900389757c6ce6c1c1b541446b
dolggul <yonunim@gmail.com>
2024-02-02 15:52:52 +0900

    refactor(settings.py, main.py): move Setting class from main.py to settings.py

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,14 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    conf_debug: bool = True
+    conf_host: str = "0.0.0.0"
+    conf_port: int = 8000
     openai_api_key: str = Field(..., env="OPENAI_API_KEY")
+
+    database_url: str
+    database_name: str
+    database_password: str
 
     class Config:
         env_file = ".env"
