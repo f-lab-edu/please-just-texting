03253e78e4f2e926a61044e9930e6f23474570b1
dolggul <yonunim@gmail.com>
2024-02-02 17:24:37 +0900

    fix(settings.py): set defulat val in user_name and user_password fields

--- app/settings.py
+++ app/settings.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     conf_port: int = 8000
     openai_api_key: str = Field(..., env="OPENAI_API_KEY")
 
-    user_name: str = Field(..., min_length=1)
-    user_password: str = Field(..., min_length=1)
+    user_name: str = Field(..., env="USER_NAME")
+    user_password: str = Field(..., env="USER_PASSWORD")
 
     class Config:
         env_file = ".env"
