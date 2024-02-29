b919a179f99f6ca658848fd916f178bcbdd68c68
dolggul2 <dolggul2@gmail.com>
2024-02-29 17:10:11 +0900

    feat(users.py): update create_user to validate data with Pydantic

--- app/models/dao/users.py
+++ app/models/dao/users.py
@@ -2,6 +2,10 @@ from typing import Optional
 
 from models import User
 from sqlalchemy.orm import Session
+from schemas import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def get_user(db: Session, user_id: int) -> Optional[User]:
@@ -12,11 +16,13 @@ def get_users(db: Session, skip: int = 0, limit: int = 100) -> list[User]:
     return db.query(User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: User) -> User:
-    db.add(user)
+def create_user(db: Session, user: UserCreate) -> User:
+    password_hash = pwd_context.hash(user.password)
+    db_user = User(name=user.name, password_hash=password_hash, email=user.user_email)
+    db.add(db_user)
     db.commit()
-    db.refresh(user)
-    return user
+    db.refresh(db_user)
+    return db_user
 
 
 def update_user(db: Session, user_id: int, user: User) -> Optional[User]:
