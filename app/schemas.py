1a6cd2232412f0b85ade52ebf3a8e63858afb95e
dolggul2 <dolggul2@gmail.com>
2024-02-29 16:51:34 +0900

    feat(schemas.py): add user schemas for validation

---
+++ app/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# create user
+class UserCreate(BaseModel):
+    name: str
+    password: str
+    user_email: EmailStr
+
+# update user
+class UpdateUser(BaseModel):
+    name: str = Field(default=None)
+    password: str = Field(default=None)
+    user_email: EmailStr = Field(default=None)
+
+# search user
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    user_email: EmailStr
+
+    class Config:
+        orm_mode = True
