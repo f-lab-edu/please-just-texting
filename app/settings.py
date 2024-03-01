ccd95aa7f3fb17801f77c6b814f3157d13c89df2
dolggul2 <dolggul2@gmail.com>
2024-03-01 21:24:03 +0900

    fix(settings.py): Remove unnecessary Settings fields
    - Remove unused user_name field
    - Remove unused user_password field

--- app/settings.py
+++ app/settings.py
@@ -8,9 +8,6 @@ class Settings(BaseSettings):
     conf_port: int = 8000
     openai_api_key: str = Field(..., env="OPENAI_API_KEY")
 
-    user_name: str = Field(..., env="USER_NAME")
-    user_password: str = Field(..., env="USER_PASSWORD")
-
     class Config:
         env_file = ".env"
 
