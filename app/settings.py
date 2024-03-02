00a8bd8294238d15a75ccd00349d74f8b0409620
dolggul2 <dolggul2@gmail.com>
2024-03-02 23:22:05 +0900

    refactor(settings.py): add database attr to validate DATABASE_URL in .env

--- app/settings.py
+++ app/settings.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     conf_host: str = "0.0.0.0"
     conf_port: int = 8000
     openai_api_key: str = Field(..., env="OPENAI_API_KEY")
+    database_url: str = Field(..., env="DATABASE_URL")
 
     class Config:
         env_file = ".env"
