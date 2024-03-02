12f9855c538e66143f9285627e39c18f62366c3c
dolggul2 <dolggul2@gmail.com>
2024-03-02 22:21:25 +0900

    refactor(dependencies.py): Improve environment variable handling
    - Ensure .env file is loaded correctly for database URL configuration.

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,11 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+# load .env file
+load_dotenv()
 
 # database engine
 DATABASE_URL = os.environ.get("DATABASE_URL")
