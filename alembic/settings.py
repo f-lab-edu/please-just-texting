ba3a778f10013c25ba2249d105e841ab1563cc16
dolggul2 <dolggul2@gmail.com>
2024-03-07 18:26:42 +0900

    refactor(type hint, remove print): add type hint and remove test print statement

--- alembic/settings.py
+++ alembic/settings.py
@@ -2,11 +2,10 @@ import sys
 from pathlib import Path
 
 
-def add_project_root_to_path():
+def add_project_root_to_path() -> None:
     PROJECT_ROOT = Path(__file__).resolve().parent.parent
     if str(PROJECT_ROOT) not in sys.path:
         sys.path.append(str(PROJECT_ROOT))
 
 
 add_project_root_to_path()
-print(sys.path)
