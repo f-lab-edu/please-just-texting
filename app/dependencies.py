03474d217615149b7cc6f36666b37f5abe916df3
dolggul2 <dolggul2@gmail.com>
2024-03-16 08:02:51 +0900

    chore(type hint): Add return type hint to depedencies.py
    - Add return type hint to get_db function

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,9 @@
 import os
+from typing import Generator
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -12,7 +14,7 @@ engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
 
 
-def get_db():
+def get_db() -> Generator[Session, None, None]:
     db = session()
     try:
         yield db
