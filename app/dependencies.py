a27636d1c6fd3726582b5073ce3be371f6e92342
dolggul2 <dolggul2@gmail.com>
2024-03-16 07:50:44 +0900

    chore(comments): Remove all comments from dependencies.py
    - Remove "load .env file" comment.
    - Remove "database engine" comment.
    - Remove "session" comment.
    - Remove "database connectin" commtent.

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,18 +4,14 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# load .env file
 load_dotenv()
 
-# database engine
 DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
-# session
 session = sessionmaker(bind=engine)
 
 
-# database connection
 def get_db():
     db = session()
     try:
