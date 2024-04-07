b34f52e5242f7fa0db145e11122c763945ecb549
dolggul2 <dolggul2@gmail.com>
2024-04-07 22:16:31 +0900

    refactor(comments): Remove authentication comments from file
    - There is no need to keep previous authentication code,
    - remove all comments about athentication from file.

--- app/utils/calender_utils.py
+++ app/utils/calender_utils.py
@@ -6,19 +6,6 @@ import pickle
 from app.settings import PROJECT_ROOT
 from googleapiclient.discovery import build
 
-"""
-============================================================
-Replaced on: 2024-4-7
-The current application does not use a web browser.
-It's impossible to perform authentication via the conventional OAuth 2.0 method.
-For future changes, leave code with comments.
-
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-============================================================
-"""
-
-
 logger = logging.getLogger()
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
@@ -26,32 +13,6 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def get_calendar_service():
     creds = None
-
-    """
-    ============================================================
-    Replaced on: 2024-4-7
-    The current application does not use a web browser.
-    It's impossible to perform authentication via the conventional OAuth 2.0 method.
-    For future changes, leave code with comments.
-
-    token_path = PROJECT_ROOT / "token.pickle"
-    if os.path.exists(token_path):
-        with open(token_path, "rb") as token:
-            creds = pickle.load(token)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-
-    ============================================================
-    """
-
     service = build("calendar", "v3", credentials=creds)
     return service
 
