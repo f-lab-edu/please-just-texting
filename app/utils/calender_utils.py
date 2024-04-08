934c7ba4831b62c09cc1ecb73f77eb40b8f54318
dolggul2 <dolggul2@gmail.com>
2024-04-08 15:10:57 +0900

    fix(authentication): Add new code for authentication using service account
    - add path about service account json file.
    
    - add account to impersonate.
    - create credentials instance from a servcie account json file.

--- app/utils/calender_utils.py
+++ app/utils/calender_utils.py
@@ -4,15 +4,21 @@ import os
 import pickle
 
 from app.settings import PROJECT_ROOT
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 logger = logging.getLogger()
 
+SERVICE_ACCOUNT_FILE = PROJECT_ROOT / "credentials.json"
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
+USER_TO_IMPERSONATE = "dolggul2@dolggul.com"
 
 
-def get_calendar_service():
-    creds = None
+def get_calendar_service() -> Request:
+    creds = Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES, subject=USER_TO_IMPERSONATE
+    )
     service = build("calendar", "v3", credentials=creds)
     return service
 
