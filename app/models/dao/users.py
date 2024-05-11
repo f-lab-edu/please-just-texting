4440a4f2ea76c86cb806b4174de07978ef472507
dolggul2 <dolggul2@gmail.com>
2024-05-11 22:26:51 +0900

    Update(async): Update db logic to be asyncronous
    - Add "get_db_user" function for DRY.
    - Add async, await keyword for asyncronous work.
    - Add cryptograph module to requirements.txt.
    - Update "update_user" function.
    
    Use AsyncSession instead of Session, db logic can work asyncronously.

--- app/models/dao/users.py
+++ app/models/dao/users.py
@@ -4,54 +4,61 @@ from app.schemas import UserCreate
 from app.schemas import UserLogin
 from fastapi import HTTPException
 from passlib.context import CryptContext
+from sqlalchemy import delete
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+async def get_db_user(db: AsyncSession, field):
+    statement = select(User).where(User.name == field)
+    result = await db.execute(statement)
+    return result.scalar()
+
+
 async def check_user_exists(db: AsyncSession, user: UserLogin):
-    db_user = db.query(User).filter(User.name == user.name).first()
+    db_user = await get_db_user(db, user.name)
     if not db_user or not pwd_context.verify(user.password, db_user.password_hash):
         raise HTTPException(status_code=401, detail="Invalid username or passowrd")
 
 
-def check_user_duplicate(db: AsyncSession, email: str):
-    db_user = db.query(User).filter(User.email == email).first()
+async def check_user_duplicate(db: AsyncSession, email: str):
+    db_user = await get_db_user(db, email)
     if db_user:
         raise HTTPException(status_code=400, detail="eamil already exists")
 
 
-def get_user(db: AsyncSession, user_id: int) -> User:
-    check_user_exists(db, user_id)
-    db_user = db_user = db.query(User).filter(User.id == user_id).first()
+async def get_user(db: AsyncSession, user_id: int) -> User:
+    await check_user_exists(db, user_id)
+    db_user = await get_db_user(db, user_id)
     return db_user
 
 
-def get_users(db: AsyncSession, skip: int = 0, limit: int = 100) -> list[User]:
-    return db.query(User).offset(skip).limit(limit).all()
+async def get_users(db: AsyncSession, skip: int = 0, limit: int = 100) -> list[User]:
+    result = await db.execute(select(User).offset(skip).limit(limit))
+    return result.scalars().all()
 
 
 async def create_user(db: AsyncSession, user: UserCreate) -> User:
-    check_user_duplicate(db, user.user_email)
+    await check_user_duplicate(db, user.user_email)
     password_hash = pwd_context.hash(user.password)
     db_user = User(name=user.name, password_hash=password_hash, email=user.user_email)
     db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    await db.commit()
+    await db.refresh(db_user)
     return db_user
 
 
-def update_user(db: AsyncSession, user_id: int, user: UpdateUser) -> User:
-    check_user_exists(db, user_id)
-    db_user = db.query(User).filter(User.id == user_id).first()
+async def update_user(db: AsyncSession, user_id: int, user: UpdateUser) -> User:
+    db_user = await db.query(User).filter(User.id == user_id).first()
     user_data = user.model_dump(exclude_unset=True)
     for key, value in user_data.items():
         setattr(db_user, key, value)
-    db.commit()
+    await db.commit()
     return db_user
 
 
-def delete_user(db: AsyncSession, user_id: int) -> None:
-    check_user_exists(db, user_id)
-    db.query(User).filter(User.id == user_id).delete()
-    db.commit()
+async def delete_user(db: AsyncSession, user_id: int) -> None:
+    await db.execute(delete(User).where(User.id == user_id))
+    await db.commit()
