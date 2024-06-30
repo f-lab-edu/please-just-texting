5d6fdd3c15d6d11b8caa811d4eafa6cc7727cb3a
dolggul2 <dolggul2@gmail.com>
2024-06-30 14:28:34 +0900

    refactor(dao): refactor "check_user-duplicate" function parameter.
    - Chage "check_user_duplicate" function to accept individual "name" and "email" parameters instead of "UserCreateModel".

--- app/models/dao/users.py
+++ app/models/dao/users.py
@@ -32,17 +32,15 @@ async def check_user_exists(db: AsyncSession, user: UserSigninModel):
     raise HTTPException(status_code=401, detail="Invalid username or password")
 
 
-async def check_user_duplicate(db: AsyncSession, user: UserCreateModel):
-    existing_user_by_name: str | None = await get_user_by_name(db=db, name=user.name)
-    existing_user_by_email: str | None = await get_user_by_email(
-        db=db, email=user.email
-    )
+async def check_user_duplicate(db: AsyncSession, name: str, email: str):
+    existing_user_by_name: str | None = await get_user_by_name(db=db, name=name)
+    existing_user_by_email: str | None = await get_user_by_email(db=db, email=email)
     if existing_user_by_name or existing_user_by_email:
         raise HTTPException(status_code=400, detail="username or email already exits")
 
 
 async def create_user(db: AsyncSession, user: UserCreateModel) -> User:
-    await check_user_duplicate(db, user)
+    await check_user_duplicate(name=user.name, email=user.email, db=db)
     password_hash = pwd_context.hash(user.password)
     db_user = User(name=user.name, password_hash=password_hash, email=user.email)
     db.add(db_user)
